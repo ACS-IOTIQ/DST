7988f35170c0a351345e5083af80b50bba9659ac
Anusha-datti <dattianusha06@gmail.com>
2026-05-27 16:23:53 +0530

    Add module description for ContactDisplayManager

--- contact_display_manager.py
+++ contact_display_manager.py
@@ -1,4 +1,5 @@
 ﻿# Render contacts on the map canvas
+# This module is responsible for drawing contact information on the map canvas. It will
 # This is a placeholder for the actual implementation, which would involve
 class ContactDisplayManager:
     def draw(self): pass
