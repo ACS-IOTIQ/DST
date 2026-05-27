49d06de672eba1d6b503fee6fcf96247f4082a26
Anusha-datti <dattianusha06@gmail.com>
2026-05-27 17:28:36 +0530

    Add clarification in ui_components.py

--- ui_components.py
+++ ui_components.py
@@ -1,3 +1,4 @@
 ﻿# Reusable Qt UI component library
+# This file is intended to be imported as a module by other UI components, and should not contain any
 class StatusBar:
     def set_message(self, msg): pass
