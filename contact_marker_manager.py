c3992a0ea8ec7ad502e6385d1e3c23399479a7db
Anusha-datti <dattianusha06@gmail.com>
2026-05-27 16:25:29 +0530

    Add module description for ContactMarkerManager

--- contact_marker_manager.py
+++ contact_marker_manager.py
@@ -1,3 +1,4 @@
 ﻿# Manage contact marker overlays
+# This module is responsible for managing the contact marker overlays on the map canvas. It will
 class ContactMarkerManager:
     def update(self): pass
