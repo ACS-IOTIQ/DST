1ec7e01d5c8d869bb95b1862a003af58edb9a5a5
Anusha-datti <dattianusha06@gmail.com>
2026-05-27 16:26:51 +0530

    module description for ContactForm

--- contactform.py
+++ contactform.py
@@ -1,3 +1,4 @@
 ﻿# Contact creation and edit form
+# This module is responsible for providing the user interface for creating and editing contacts. It will
 class ContactForm:
     def exec(self): pass
